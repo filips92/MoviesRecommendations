2888ef37cc401ddcbcd4b794c7560ac514c1f2f6
Filip <filsmu@wp.pl>
2015-12-16 22:50:22 +0100

    Loading list of evaluations with non null grades

--- MovieRecommender/MovieRecommender/Program.cs
+++ MovieRecommender/MovieRecommender/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using TMDbLib.Client;
 using TMDbLib.Objects.Movies;
+using System.IO;
 
 namespace MovieRecommender
 {
@@ -16,6 +17,28 @@ namespace MovieRecommender
             var client = new TMDbClient(ConfigurationManager.AppSettings["TMDbAPIKey"]);
             var test = new SimpleMovie(client.GetMovie(47964, MovieMethods.Credits | MovieMethods.Keywords | MovieMethods.Reviews));
             int a = 2;
+
+            var reader = new StreamReader(File.OpenRead("../../AppData/evaluations.csv"));
+
+            List<Evaluation> evaluations = new List<Evaluation>();
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+                double[] doubleArray = new double[values.Count()];
+                Evaluation evaluation;
+                if (values[values.Count() - 1] != "NULL") {
+                    evaluation = new Evaluation(){
+                        Id = Convert.ToInt32(values[0]),
+                        PersonId = Convert.ToInt32(values[1]),
+                        MovieId = Convert.ToInt32(values[2]),
+                        Grade = Convert.ToInt32(values[3])
+                    };
+                    evaluations.Add(evaluation);
+                }                   
+            }
+           
         }
     }
 }
