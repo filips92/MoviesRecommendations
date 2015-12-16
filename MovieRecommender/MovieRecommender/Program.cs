b7b481a3b4b2fb7326ecc77486e3c8bdf6a017d4
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2015-12-16 16:28:57 +0100

    added library to connect api and a simple movie wrapper

--- MovieRecommender/MovieRecommender/Program.cs
+++ MovieRecommender/MovieRecommender/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
 
 namespace MovieRecommender
 {
@@ -10,6 +13,9 @@ namespace MovieRecommender
     {
         static void Main(string[] args)
         {
+            var client = new TMDbClient(ConfigurationManager.AppSettings["TMDbAPIKey"]);
+            var test = new SimpleMovie(client.GetMovie(47964, MovieMethods.Credits | MovieMethods.Keywords | MovieMethods.Reviews));
+            int a = 2;
         }
     }
 }
---
+++ MovieRecommender/MovieRecommender/SimpleMovie.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TMDbLib.Objects.Movies;
+
+namespace MovieRecommender
+{
+    class SimpleMovie
+    {
+        public SimpleMovie(Movie tmdbMovie)
+        {
+            this.Budget = tmdbMovie.Budget;
+            this.MovieId = tmdbMovie.Id;
+            this.Popularity = tmdbMovie.Popularity;
+            this.Title = tmdbMovie.Title;
+            this.VoteAverage = tmdbMovie.VoteAverage;
+
+            var director = tmdbMovie.Credits.Crew.FirstOrDefault(c => c.Job == "Director");
+            if (director != null)
+            {
+                this.Director = director.Name;
+                this.DirectorId = director.Id;
+            }
+            else
+            {
+                this.Director = "N/A";
+                this.DirectorId = -1;
+            }
+
+            var mainLanguage = tmdbMovie.SpokenLanguages.FirstOrDefault();
+            if (mainLanguage != null)
+            {
+                this.MainLanguage = mainLanguage.Name;
+                var culture =
+                    CultureInfo.GetCultures(CultureTypes.AllCultures)
+                        .FirstOrDefault(c => c.TwoLetterISOLanguageName == mainLanguage.Iso_639_1);
+                if (culture != null)
+                {
+                    this.MainLanguageId = culture.LCID;
+                }
+                else
+                {
+                    this.MainLanguageId = -1;
+                }
+            }
+
+            var mainActor = tmdbMovie.Credits.Cast.OrderBy(c => c.Order).FirstOrDefault();
+            if (mainActor != null)
+            {
+                this.MainActor = mainActor.Name;
+                this.MainActorId = mainActor.Id;
+            }
+            else
+            {
+                this.MainActor = "N/A";
+                this.MainActorId = -1;
+            }
+
+            var releaseDate = tmdbMovie.ReleaseDate;
+            if (releaseDate.HasValue)
+            {
+                this.Year = releaseDate.Value.Year;
+            }
+            else
+            {
+                this.Year = -1;
+            }
+        }
+
+        public long Budget { get; set; }
+        public string Director { get; set; }
+        public int DirectorId { get; set; }
+        public string MainLanguage { get; set; }
+        public int MainLanguageId { get; set; }
+        public int MovieId { get; set; }
+        public string MainActor { get; set; }
+        public int MainActorId { get; set; }
+        public double Popularity { get; set; }
+        public double VoteAverage { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} ({1}) by {2}", this.Title, this.Year, this.Director);
+        }
+
+        public double[] ToVector()
+        {
+            return new double[]
+            {
+                this.Budget,
+                this.DirectorId,
+                this.MainLanguageId,
+                this.MainActorId,
+                this.Popularity,
+                this.VoteAverage,
+                this.Year
+            };
+        }
+
+        public string ToVectorString()
+        {
+            return String.Join(" ", this.ToVector());
+        }
+    }
+}
